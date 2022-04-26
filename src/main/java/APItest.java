35c4b9aae9d80e3607d265050a399d8591878e3f
Carolina <45121184+RealAgnetha@users.noreply.github.com>
2022-04-26 20:03:51 +0200

    add exemplatory class and api as setup for swagger introduction

---
+++ src/main/java/APItest.java
@@ -0,0 +1,34 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@RestController
+public class APItest {
+
+    ConcurrentMap<String, Ding> dings = new ConcurrentHashMap<>();
+
+    @GetMapping("/{id}")
+    public Ding getDing(String id) {
+        return dings.get(id);
+    }
+
+    @GetMapping("/")
+    public List<Ding> getAllDings() {
+        return new ArrayList<>(dings.values());
+    }
+
+    @PostMapping("/")
+    public Ding addDing(Ding ding) {
+        dings.put(ding.getId(), ding);
+        return ding;
+    }
+
+
+}
+
+
---
+++ src/main/java/Ding.java
@@ -0,0 +1,26 @@
+public class Ding {
+
+    String id;
+    String attribute;
+
+    public Ding(String id, String attribute) {
+        this.id = id;
+        this.attribute = attribute;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAttribute() {
+        return attribute;
+    }
+
+    public void setAttribute(String attribute) {
+        this.attribute = attribute;
+    }
+}
