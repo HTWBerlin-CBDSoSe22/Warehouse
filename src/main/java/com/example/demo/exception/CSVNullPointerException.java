f0f82fb56089fc294869e6f2fb203de3993ad673
Carolina <45121184+RealAgnetha@users.noreply.github.com>
2022-05-03 23:14:11 +0200

    kbe-30 merge kbe-20 into branch

---
+++ src/main/java/com/example/demo/exception/CSVNullPointerException.java
@@ -0,0 +1,11 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.INTERNAL_SERVER_ERROR, reason = "no components found")
+public class CSVNullPointerException extends RuntimeException{
+    public CSVNullPointerException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/demo/model/Component.java
@@ -0,0 +1,38 @@
+package com.example.demo.model;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Component {
+    public String name;
+    public String price;
+    public String height;
+    public String weight;
+    public String color;
+    public String countryOfOrigin;
+    public String grade;
+    public String category;
+    public String classification;
+    public String harvestSeason;
+
+
+    @Override
+    public String toString() {
+        return "Component{" +
+                "name='" + name + '\'' +
+                ", price='" + price + '\'' +
+                ", height='" + height + '\'' +
+                ", weight='" + weight + '\'' +
+                ", color='" + color + '\'' +
+                ", countryOfOrigin='" + countryOfOrigin + '\'' +
+                ", grade='" + grade + '\'' +
+                ", category='" + category + '\'' +
+                ", classification='" + classification + '\'' +
+                ", harvestSeason='" + harvestSeason + '\'' +
+                '}';
+    }
+
+
+
+
+}
