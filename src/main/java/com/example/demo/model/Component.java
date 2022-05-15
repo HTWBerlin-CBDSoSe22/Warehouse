2368527c0c1fe43a01815144d6cd56f824f17de7
Carolina <45121184+RealAgnetha@users.noreply.github.com>
2022-05-15 18:38:14 +0200

    KBE-32-database-local add jpa

--- src/main/java/com/example/demo/model/Component.java
+++ src/main/java/com/example/demo/model/Component.java
@@ -1,6 +1,14 @@
 package com.example.demo.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
 public class Component {
+
+    private UUID id;
     public String name;
     public String price;
     public String height;
@@ -12,20 +20,106 @@ public class Component {
     public String classification;
     public String harvestSeason;
 
-    @Override
-    public String toString() {
-        return "Component{" +
-                "name='" + name + '\'' +
-                ", price='" + price + '\'' +
-                ", height='" + height + '\'' +
-                ", weight='" + weight + '\'' +
-                ", color='" + color + '\'' +
-                ", countryOfOrigin='" + countryOfOrigin + '\'' +
-                ", grade='" + grade + '\'' +
-                ", category='" + category + '\'' +
-                ", classification='" + classification + '\'' +
-                ", harvestSeason='" + harvestSeason + '\'' +
-                '}';
+    public Component(UUID id, String name, String price, String height, String weight, String color, String countryOfOrigin, String grade, String category, String classification, String harvestSeason) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.height = height;
+        this.weight = weight;
+        this.color = color;
+        this.countryOfOrigin = countryOfOrigin;
+        this.grade = grade;
+        this.category = category;
+        this.classification = classification;
+        this.harvestSeason = harvestSeason;
     }
 
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    @Id
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getCountryOfOrigin() {
+        return countryOfOrigin;
+    }
+
+    public void setCountryOfOrigin(String countryOfOrigin) {
+        this.countryOfOrigin = countryOfOrigin;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getClassification() {
+        return classification;
+    }
+
+    public void setClassification(String classification) {
+        this.classification = classification;
+    }
+
+    public String getHarvestSeason() {
+        return harvestSeason;
+    }
+
+    public void setHarvestSeason(String harvestSeason) {
+        this.harvestSeason = harvestSeason;
+    }
 }
