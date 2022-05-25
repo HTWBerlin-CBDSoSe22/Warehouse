87fd239400e64875cfbd8ada2730e14a99e6d6b3
RealAgnetha <carolinatrack@googlemail.com>
2022-05-25 18:52:14 +0200

    fix packaging mistake

--- src/test/java/com/example/demo/controller/ComponentControllerTest.java
+++ src/test/java/com/example/demo/controller/ComponentControllerTest.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import com.example.demo.controller.ComponentController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
--- src/test/java/com/example/demo/controller/ProductControllerTest.java
+++ src/test/java/com/example/demo/controller/ProductControllerTest.java
@@ -1,6 +1,5 @@
-package com.example.demo;
+package com.example.demo.controller;
 
-import com.example.demo.controller.ProductController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
