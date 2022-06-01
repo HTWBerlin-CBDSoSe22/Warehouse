c9c69968bd3dd36b883392eb1b33bbb6c67df74b
RealAgnetha <carolinatrack@googlemail.com>
2022-06-01 19:23:25 +0200

    KBE-44-finish-endpoints start with the gist of returning status code when id not found

--- src/main/java/com/example/demo/controller/ComponentController.java
+++ src/main/java/com/example/demo/controller/ComponentController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.jpa.ComponentRepository;
 import com.example.demo.model.Component;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,8 +23,16 @@ public class ComponentController {
     }
 
     @GetMapping(path = "/{componentId}")
-    Optional<Component> findComponentById(@PathVariable("componentId") long componentId){
-        return (componentRepository.findById(componentId));
+    ResponseEntity<Component> findComponentById(@PathVariable("componentId") long componentId) throws Exception {
+        // boolean componentExists = componentRepository.existsById(componentId);
+        // if (componentExists) {
+        Optional<Component> newComponent = componentRepository.findById(componentId);
+        if (newComponent.isEmpty()) { //true = null, kein objekt
+            throw new Exception();
+        } //sorry all over the place aber uebung ist vorbei und muss los
+        return ResponseEntity.badRequest().build(); //hier passender response status
+        //see zb. stackabuse.com how to return http status codes in a spring boot application
     }
+
 
 }
