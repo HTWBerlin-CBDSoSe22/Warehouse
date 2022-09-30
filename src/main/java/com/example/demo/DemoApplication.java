7657e71b3237ee170d0d56a183954365235ef6bb
LuckyLuke16 <83580682+LuckyLuke16@users.noreply.github.com>
2022-09-30 17:09:50 +0200

    - clean up code

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,6 @@
 package com.example.demo;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
